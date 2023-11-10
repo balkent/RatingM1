f1091dc500a18e30ba008511d5dffc9127de5c3c
Quentin BALCEREK <quentin.balcerek@gmail.com>
2023-11-10 20:18:20 +0100

    feat(supplement): add array in dictionary

--- src/Dictionary/SupplementTypeDictionary.php
+++ src/Dictionary/SupplementTypeDictionary.php
@@ -7,4 +7,10 @@ class SupplementTypeDictionary
     public const SUPPLEMENT_TYPE_MALUS = 'malus';
     public const SUPPLEMENT_TYPE_BONUS = 'bonus';
     public const SUPPLEMENT_TYPE_ULTRA_BONUS = 'ultra-bonus';
+
+    public const ALL = [
+        'SUPPLEMENT_TYPE_MALUS' => self::SUPPLEMENT_TYPE_MALUS,
+        'SUPPLEMENT_TYPE_BONUS' => self::SUPPLEMENT_TYPE_BONUS,
+        'SUPPLEMENT_TYPE_ULTRA_BONUS' => self::SUPPLEMENT_TYPE_ULTRA_BONUS,
+    ];
 }
