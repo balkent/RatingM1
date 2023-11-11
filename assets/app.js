d2a2a351f902a6039a1a588175850a26c4fb1630
Quentin BALCEREK <quentin.balcerek@gmail.com>
2023-11-11 16:05:04 +0100

    fix(core): remove useless code js

--- assets/app.js
+++ assets/app.js
@@ -13,9 +13,6 @@ require('bootstrap');
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
-require('bootstrap/js/dist/popover');
+// require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
 import './styles/global.scss';
